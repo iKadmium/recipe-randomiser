8e81f2c345a743cc4d3260a9dd56e48268a19211
Jesse Higginson <jesse.d.higginson@gmail.com>
2025-03-31 04:12:27 +0000

    Add check step

--- src/routes/ingredients/new/+page.svelte
+++ src/routes/ingredients/new/+page.svelte
@@ -4,34 +4,28 @@
 	import { writable } from 'svelte/store';
 	import IngredientEditor from '../../../components/IngredientEditor.svelte';
 
-	const ingredient = writable<Ingredient>({
+	const initial: Ingredient = {
+		fresh: false,
 		name: '',
-		unit: ''
-	});
+		unit: 'things'
+	};
 
-	async function handleSubmit(e: SubmitEvent) {
-		e.preventDefault();
-		if ($ingredient) {
-			const resp = await fetch('/api/ingredient', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify($ingredient)
-			});
-			if (!resp.ok) {
-				console.error('Failed to save ingredient:', resp.statusText);
-				return;
-			}
-			await goto('/ingredients');
+	async function handleSubmit(ingredient: Ingredient) {
+		const resp = await fetch('/api/ingredient', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(ingredient)
+		});
+		if (!resp.ok) {
+			console.error('Failed to save ingredient:', resp.statusText);
+			return;
 		}
+		await goto('/ingredients');
 	}
 </script>
 
 <h1>New Ingredient</h1>
 
-{#if $ingredient}
-	<IngredientEditor {ingredient} on:submit={handleSubmit} />
-{:else}
-	<p>Loading...</p>
-{/if}
+<IngredientEditor ingredient={initial} onSubmit={handleSubmit} />
