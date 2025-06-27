c2d394180df73a365bc9896fd579f9488bbe718a
Jesse Higginson <kadmium@gmail.com>
2025-06-27 12:13:26 +0000

    Fix test

--- src/lib/util.test.ts
+++ src/lib/util.test.ts
@@ -2,11 +2,11 @@ import { getFirstDayOfNextMonth } from './util';
 import { expect, test, describe } from 'vitest';
 
 describe('util', () => {
-	describe('getfirstSaturdayOfNextMonth', () => {
-		test('should return the last Friday of the month', () => {
+	describe('getFirstDayOfNextMonth', () => {
+		test('should return the first Friday of the next month', () => {
 			const date = new Date('2025-03-01');
-			const expected = new Date('2025-04-05');
-			const result = getFirstDayOfNextMonth(date);
+			const expected = new Date('2025-04-04');
+			const result = getFirstDayOfNextMonth(date, "Friday");
 			expect(result).toEqual(expected);
 		});
 	});
