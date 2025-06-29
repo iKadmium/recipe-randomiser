c67b38b6e5d6e46773678c8009b2c068a853fdbc
Jesse Higginson <jesse.d.higginson>
2025-06-29 01:36:09 +0000

    Added etag safety

--- src/lib/server/netlifyBlobsDataSource.ts
+++ src/lib/server/netlifyBlobsDataSource.ts
@@ -1,10 +1,9 @@
-import { getStore } from '@netlify/blobs';
+import { getStore, type GetWithMetadataResult } from '@netlify/blobs';
 import type { Database } from '../models/database';
 import type { DataSource, Named } from './types';
 
 export class NetlifyBlobsDataSource<T extends Named<K>, K extends string = 'name'>
-	implements DataSource<T, K>
-{
+	implements DataSource<T, K> {
 	private store: ReturnType<typeof getStore>;
 	private key: string;
 	private keyProperty: K;
@@ -23,13 +22,26 @@ export class NetlifyBlobsDataSource<T extends Named<K>, K extends string = 'name
 		return data[this.keyProperty];
 	}
 
+	private async getDataRaw(): Promise<{ data: string } & GetWithMetadataResult | null> {
+		const data = await this.store.getWithMetadata(this.key, { type: 'text', consistency: 'strong' });
+		return data;
+	}
+
+	private async getAllWithMetadata(): Promise<{ data: Database<T> } & GetWithMetadataResult> {
+		const data = await this.store.getWithMetadata(this.key, { type: 'text', consistency: 'strong' });
+		if (!data) {
+			return { data: {} as Database<T>, metadata: {} };
+		}
+		return { data: JSON.parse(data.data) as Database<T>, metadata: data.metadata };
+	}
+
 	public async getAll(): Promise<Database<T>> {
 		try {
-			const data = await this.store.get(this.key, { type: 'text' });
+			const data = await this.getAllWithMetadata();
 			if (!data) {
 				return {};
 			}
-			return JSON.parse(data as string) as Database<T>;
+			return data.data;
 		} catch (error) {
 			console.error('Error reading from Netlify Blobs:', error);
 			return {};
@@ -46,10 +58,10 @@ export class NetlifyBlobsDataSource<T extends Named<K>, K extends string = 'name
 
 	public async post(data: T): Promise<void> {
 		try {
-			const all = await this.getAll();
+			const all = await this.getAllWithMetadata();
 			const id = this.getKeyFromData(data);
-			all[id] = data;
-			await this.store.setJSON(this.key, all);
+			all.data[id] = data;
+			this.store.setJSON(this.key, all.data, { onlyIfMatch: all.etag });
 		} catch (error) {
 			console.error('Error writing to Netlify Blobs:', error);
 			throw error;
@@ -57,30 +69,30 @@ export class NetlifyBlobsDataSource<T extends Named<K>, K extends string = 'name
 	}
 
 	public async put(key: string, data: T): Promise<void> {
-		const all = await this.getAll();
-		if (key in all) {
-			all[key] = data;
-			await this.store.setJSON(this.key, all);
+		const all = await this.getAllWithMetadata();
+		if (key in all.data) {
+			all.data[key] = data;
+			await this.store.setJSON(this.key, all.data, { onlyIfMatch: all.etag });
 		} else {
 			throw new Error(`No data found for id: ${key}`);
 		}
 	}
 
 	public async delete(key: string): Promise<void> {
-		const all = await this.getAll();
-		if (key in all) {
-			delete all[key];
-			await this.store.set(this.key, JSON.stringify(all));
+		const all = await this.getAllWithMetadata();
+		if (key in all.data) {
+			delete all.data[key];
+			await this.store.setJSON(this.key, all.data, { onlyIfMatch: all.etag });
 		}
 	}
 
 	public async putMany(data: Database<T>): Promise<void> {
 		try {
-			const all = await this.getAll();
+			const all = await this.getAllWithMetadata();
 			for (const key in data) {
-				all[key] = data[key];
+				all.data[key] = data[key];
 			}
-			await this.store.setJSON(this.key, all);
+			await this.store.setJSON(this.key, all.data, { onlyIfMatch: all.etag });
 		} catch (error) {
 			console.error('Error writing multiple entries to Netlify Blobs:', error);
 			throw error;
